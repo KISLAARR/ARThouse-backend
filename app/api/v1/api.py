f08641f1a03a3f1bd38d8b286ecf24295a37ecee
marrriiia <silkomaria2@gmail.com>
2026-04-02 15:40:42 +0700

    Update api.py

--- app/api/v1/api.py
+++ app/api/v1/api.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter
-from app.api.v1.endpoints import auth, users, apartments, surveys, snapshots
+from app.api.v1.endpoints import auth, users, apartments, surveys, snapshots, tasks
 
 api_router = APIRouter()
 
@@ -7,4 +7,5 @@ api_router.include_router(auth.router)
 api_router.include_router(users.router)
 api_router.include_router(apartments.router)
 api_router.include_router(surveys.router)
-api_router.include_router(snapshots.router)  # ← добавить
+api_router.include_router(snapshots.router)
+api_router.include_router(tasks.router) 
