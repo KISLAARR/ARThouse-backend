75d30b6174d9bec2a8e640cd886c68c9dfe2e696
marrriiia <silkomaria2@gmail.com>
2026-04-22 23:57:45 +0700

    Create __init__.py

---
+++ app/models/__init__.py
@@ -0,0 +1,7 @@
+from app.models.user import User
+from app.models.apartment import Apartment
+from app.models.survey import Survey
+from app.models.room import Room
+from app.models.task import Task
+from app.models.household_member import HouseholdMember
+from app.models.apartment_snapshot import ApartmentSnapshot
