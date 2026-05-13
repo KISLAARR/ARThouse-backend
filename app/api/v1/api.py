247de04d096aaed81369f6df546523e0af441d9f
marrriiia <silkomaria2@gmail.com>
2026-05-14 00:09:58 +0700

    Работа с каждым роутером

--- app/api/v1/api.py
+++ app/api/v1/api.py
@@ -1,12 +1,54 @@
 from fastapi import APIRouter
-from app.api.v1.endpoints import auth, users, apartments, surveys, snapshots, tasks, rooms
+
+from app.api.v1.endpoints import (
+    auth,
+    users,
+    apartments,
+    surveys,
+    snapshots,
+    tasks,
+    rooms
+)
 
 api_router = APIRouter()
 
-api_router.include_router(auth.router)
-api_router.include_router(users.router)
-api_router.include_router(apartments.router)
-api_router.include_router(surveys.router)
-api_router.include_router(snapshots.router)
-api_router.include_router(tasks.router)
-api_router.include_router(rooms.router)
+api_router.include_router(
+    auth.router,
+    prefix="/auth",
+    tags=["Auth"]
+)
+
+api_router.include_router(
+    users.router,
+    prefix="/users",
+    tags=["Users"]
+)
+
+api_router.include_router(
+    apartments.router,
+    prefix="/apartments",
+    tags=["Apartments"]
+)
+
+api_router.include_router(
+    surveys.router,
+    prefix="/surveys",
+    tags=["Surveys"]
+)
+
+api_router.include_router(
+    snapshots.router,
+    prefix="/snapshots",
+    tags=["Snapshots"]
+)
+
+api_router.include_router(
+    tasks.router,
+    prefix="/tasks",
+    tags=["Tasks"]
+)
+
+api_router.include_router(
+    rooms.router,
+    tags=["Rooms"]
+)
